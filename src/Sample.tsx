ea3c36d55f5f21de5b4d559d742b9749c5b6decc
Gayathri Rajendran <gayathri.rajendran@zafin.com>
2024-02-13 05:32:15 +0530

    same

--- src/Sample.tsx
+++ src/Sample.tsx
@@ -1,37 +1,51 @@
-import { Menu } from "primereact/menu"
-import { useEffect } from "react"
-import { Link, Outlet } from "react-router-dom"
+import { Menu } from "primereact/menu";
+import { MegaMenu } from "primereact/megamenu";
+import { useEffect } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
-
-const items = [{
-    id: 'home',
-    url: 'contact',
-    label: 'Test'
-  }, {
-    id: 'contact',
-    url: 'sample2',
-    label: 'Test2'
-  }]
+const items = [
+  {
+    id: "home",
+    url: "/contact",
+    label: "Test",
+  },
+  {
+    id: "contact",
+    url: "/sample2",
+    label: "Test2",
+  },
+];
 
 export const Sample = () => {
-    useEffect(() => {
-        console.log('first time');
-      }, [])
-    return (
-        <>
-            <div>Sample</div>
-            <>
-                <ul>
-                    <li>
-                        <Link to="/contact">Test</Link>
-                    </li>
-                    <li>
-                        <Link to="/sample2">Test2</Link>
-                    </li>
-                </ul>
-            </>
-            <Menu model={items} />
-            <Outlet></Outlet>
-        </>
-    )
-}
+  const nav = useNavigate();
+  useEffect(() => {
+    console.log("first time");
+  }, []);
+  const items2 = items.map((item) => ({
+    ...item,
+    command: () => nav(item.url),
+  }));
+  return (
+    <>
+      <div>Sample</div>
+      <>
+        <ul>
+          <li>
+            <Link to="/contact">Test</Link>
+          </li>
+          <li>
+            <Link to="/sample2">Test2</Link>
+          </li>
+        </ul>
+      </>
+
+      <b>Menu</b>
+      <Menu model={items2} />
+
+      <br />
+      <b>Mega</b>
+      <MegaMenu orientation="vertical" model={items2} />
+      <Outlet></Outlet>
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import "primeflex/primeflex.css";
+import "primeicons/primeicons.css";
+import "primereact/resources/primereact.css";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
